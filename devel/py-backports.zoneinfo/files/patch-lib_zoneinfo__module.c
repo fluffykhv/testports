0399c65a2a7183ef9b4d1a75b361d16a80ce7e63
Dima Panov <fluffy@FreeBSD.org>
2022-06-25 23:11:33 +0300

    devel/py-backports.zoneinfo: minimize patch

--- devel/py-backports.zoneinfo/files/patch-lib_zoneinfo__module.c
+++ devel/py-backports.zoneinfo/files/patch-lib_zoneinfo__module.c
@@ -1,21 +1,5 @@
 --- lib/zoneinfo_module.c.orig	2020-06-23 13:47:53 UTC
 +++ lib/zoneinfo_module.c
-@@ -594,11 +594,11 @@ zoneinfo_fromutc(PyObject *obj_self, PyObject *dt)
-             PyObject *kwargs = PyDict_New();
-             PyObject *one = NULL;
- 
--#ifdef ATLEAST_37
-+//#ifdef ATLEAST_37
-             one = PyLong_FromLong(1);
--#else
--            one = _PyLong_One;
--#endif
-+//#else
-+//            one = _PyLong_One;
-+//#endif
- 
-             Py_DECREF(tmp);
-             if (args == NULL || kwargs == NULL || replace == NULL ||
 @@ -617,7 +617,7 @@ zoneinfo_fromutc(PyObject *obj_self, PyObject *dt)
              Py_DECREF(args);
              Py_DECREF(kwargs);
